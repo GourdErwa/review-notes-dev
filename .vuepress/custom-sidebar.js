f26cb0fadacb2c74f0c0c9f076c8c2b7aedd74cf
GourdErwa <gourderwa@163.com>
2019-12-26 11:19:37 +0800

    update docs

--- .vuepress/custom-sidebar.js
+++ .vuepress/custom-sidebar.js
@@ -3,6 +3,7 @@ module.exports =
     {
         '/language/java-concurrency/': require("../language/java-concurrency/_sidebar"),
         '/language/scala-basis/': require("../language/scala-basis/_sidebar"),
-        '/framework/flink-basis/': require("../framework/flink-basis/_sidebar")
+        '/framework/flink-basis/': require("../framework/flink-basis/_sidebar"),
+        '/framework/redis/': require("../framework/redis/_sidebar")
     }
 ;
