00583501f5cde45e9d811d0f8932f1dc454d9c6e
GourdErwa <gourderwa@163.com>
2020-01-03 18:47:41 +0800

    update docs

--- language/java-jvm/_sidebar.js
+++ language/java-jvm/_sidebar.js
@@ -4,7 +4,8 @@ module.exports = [
         collapsable: false,
         sidebarDepth: 1,
         children: [
-            ['运行时内存数据区域', '运行时内存数据区域']
+            ['运行时内存数据区域', '运行时内存数据区域'],
+            ['对象的创建与访问过程', '对象的创建与访问过程']
         ]
     }
 ];
