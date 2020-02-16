24464f035b4ff97d7d24e6a750e8ab3040846b40
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-16 07:17:05 +0000

    Deploy GourdErwa/review-notes-dev to github.com/GourdErwa/review-notes-dev.git:gh-pages

--- assets/js/195.5f4e3bbd.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[195],{74:function(n,w,o){}}]);
---
+++ assets/js/205.33cd2d8d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[205],{74:function(n,w,o){}}]);
